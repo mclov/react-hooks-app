7c1e770001f987ede09f606839144edbe8dd7f2c
Michael Clovis <michaelclovis@protonmail.com>
2019-12-19 08:36:04 -0800

    Add Todo component and checkbox

---
+++ src/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItem';
+import Checkbox from '@material-ui/core/Checkbox';
+
+export default function Todo({ task, completed }) {
+  return (
+    <ListItem>
+      <Checkbox tabIndex={-1} checked={completed} />
+      <ListItemText>
+        {task}
+      </ListItemText>
+    </ListItem>
+  )
+}
